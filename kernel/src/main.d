f24ab43bc457272bfa09311e78f4ff72f6d05f01
RaidTheWeb <therealraidtheweb@gmail.com>
2022-09-01 20:25:05 +1200

    reinit gitignore

--- kernel/src/main.d
+++
@@ -1,3 +0,0 @@
-src/main.o: src/main.c src/include/limine.h src/include/mm/mm.h \
- src/include/mm/pmm.h src/include/lib/assert.h src/include/lib/stdio.h \
- src/include/lib/string.h src/include/serial.h
